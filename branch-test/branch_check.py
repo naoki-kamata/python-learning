f843689621179f2e7ff422a00e45441b4bfa4ee2
LAPTOP-QNKNECCU\naoki-tech <b.t.bien35@gmail.com>
2026-06-19 13:43:56 +0900

    sample関数に引数の整数判定を追加

--- branch-test/branch_check.py
+++ branch-test/branch_check.py
@@ -1,10 +1,14 @@
 def sample(number):
-    total = 0
-    for num in range(1, number + 1):
-        total += num
-    return total
+    is_int = isinstance(number, int)
+    if not is_int:
+        return f"{number}は数値ではありません"
+    else:
+        total = 0
+        for num in range(1, number + 1):
+            total += num
+        return total
 
 
 if __name__ == "__main__":
-    res = sample(5)
+    res = sample("5")
     print(res)
